807466812fa974dc4ccb02d0e38e887dbcc66a64
kurves <kavssimply@gmail.com>
2023-10-01 13:03:08 +0300

    configure required hosts

--- screenrecords/settings.py
+++ screenrecords/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY",)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hng-ce-backend.onrender.com','127.0.0.1:8000']
 
 
 # Application definition
