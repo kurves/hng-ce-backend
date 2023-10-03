a4af79f06865093e5ad979f5e5f4f70e36f100e7
kurves <kavssimply@gmail.com>
2023-10-03 09:39:05 +0300

    create swagger docs

--- screenrecords/settings.py
+++ screenrecords/settings.py
@@ -125,7 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
