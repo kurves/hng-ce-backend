7691b0fda929c4a8c621c23f0fb0888722be73d8
kurves <kavssimply@gmail.com>
2023-10-03 12:09:14 +0300

    Create swagger documentantion

--- screenrecords/settings.py
+++ screenrecords/settings.py
@@ -138,7 +138,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-STATIC_URL = os.path.join(BASE_DIR, "static/")
+STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
