9f5bd377001eb4d889549f3a050faf2c5d7319ca
kurves <kavssimply@gmail.com>
2023-10-03 12:42:36 +0300

    collect static files

--- screenrecords/settings.py
+++ screenrecords/settings.py
@@ -139,7 +139,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
+STATIC_ROOT = BASE_DIR/ "staticfiles/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
