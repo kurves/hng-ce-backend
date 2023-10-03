1038f08c24cfae581e281b36d68706811caf0afa
kurves <kavssimply@gmail.com>
2023-10-03 13:01:20 +0300

    collect static file

--- screenrecords/settings.py
+++ screenrecords/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'screenapp',
     'rest_framework',
     'drf_yasg',
-    'whitenoise.runserver_nostatic',
+  
 
 ]
 
@@ -58,13 +58,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 
@@ -140,7 +141,21 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR/ "staticfiles/"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
+
+
+
 
 
 
